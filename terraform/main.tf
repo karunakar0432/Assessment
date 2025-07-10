6943a0c4729471acc19995e4d450fa5176cee289
karunakar0432 <92034834+karunakar0432@users.noreply.github.com>
2025-07-10 21:56:43 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
+module "networking" {
+  source              = "./modules/vnet"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  vnet_name           = var.vnet_name
+  vnet_address_space  = var.vnet_address_space
+  subnets             = var.subnets
+}
